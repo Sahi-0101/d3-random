86845c2c2a0285cf9e7d9fec53f7b691c5b2c2ea
Sahi-0101 <79036475+Sahi-0101@users.noreply.github.com>
2022-01-15 15:47:27 +0530

    Update chisquare.js

--- src/chisquare.js
+++ src/chisquare.js
@@ -1,1 +1,6 @@
+import defaultSource from "./defaultSource.js";
+import normal from "./normal.js";
 
+export default (function sourceRandomChi(source) {
+  var randomNormal = normal.source(source)();
+  function randomChi(k, theta) {
